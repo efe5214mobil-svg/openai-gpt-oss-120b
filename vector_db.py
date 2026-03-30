a4bc99ada0ff4f2afe3042f481dd6808f2e3a3b9
efe5214mobil-svg <efe5214mobil@gmail.com>
2026-03-30 20:12:00 +0300

    Update vector_db.py

--- vector_db.py
+++ vector_db.py
@@ -1,35 +1,12 @@
-# rag.py
-from groq import Groq
-import os
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+def load_vector_db():
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
-def okul_asistani_sorgula(soru, vector_db):
-    # Vektör DB’den en yakın 3 dokümanı al
-    docs = vector_db.similarity_search(soru, k=3)
-
-    # Dokümanları bağlam olarak birleştir
-    baglam = "\n\n".join([doc.page_content for doc in docs])
-
-    # AI çağrısı
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "system",
-                "content": "Cevabını sadece verilen bağlama göre ver."
-            },
-            {
-                "role": "user",
-                "content": f"Bağlam:\n{baglam}\n\nSoru: {soru}"
-            }
-        ],
-        model="openai/gpt-oss-120b",
-        temperature=0,
+    db = Chroma(
+        persist_directory="okul_asistani_gpt_db",
+        embedding_function=embeddings
     )
 
-    cevap = chat_completion.choices[0].message.content
-
-    # Kaynakları döndür
-    kaynaklar = [doc.page_content[:200] for doc in docs]
-
-    return cevap, kaynaklar
+    return db
